27623236af8b782479eabd25891a4133a6a5570d
isa56 <isadoraadriana@gmail.com>
2021-11-23 17:26:34 -0300

    Arrumando branch main e atualizando ReadMe

--- src/components/Primeiro.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { SafeAreaView, Text, StyleSheet } from 'react-native'
-import Estilo from './estilo'
-
-export default () =>    <Text style={Estilo.bigTextStyle}>Teste!!</Text>
