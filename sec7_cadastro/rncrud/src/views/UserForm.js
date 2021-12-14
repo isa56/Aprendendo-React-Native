ad0c2fa31b803ee133ad9a7db5fa06e2fd78b98b
isa56 <isadoraadriana@gmail.com>
2021-12-14 16:49:41 -0300

    Adicionando arquivos iniciais e Navegação Stack funcional

---
+++ sec7_cadastro/rncrud/src/views/UserForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+// import styles from './styles';
+
+export default (props) => {
+
+    return(
+        <Text>
+            UserForm
+        </Text>
+    );
+}
